20c36a9f8a10c045cc3e815b2fd6ee479dd50f98
Artem Kezlya <artem.kez.work@gmail.com>
2021-05-05 13:32:28 +0300

    task3 is done

--- task3.js
+++ task3.js
@@ -1,7 +1,14 @@
-// function superfunc(...args) {
-//     return args;
-// }
-
 function superfunc() {
-    return [...arguments];
+    let arr = [];
+    return function (...arguments) {
+        arr = [...arr, ...arguments];
+        return arr;
+    }
 }
+
+let a = superfunc()
+console.log(a(1, 2, 3))
+
+let b = superfunc()
+console.log(b(1, 2))
+console.log(a(5))
